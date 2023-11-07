6ce13f002926f826db33bac23dbbb11a3a137502
VladBrudaru <vlad.brudaru@yahoo.com>
2023-11-07 09:00:41 +0200

    Am facut tema 14

---
+++ 14 august/Js_functions.js
@@ -0,0 +1,7 @@
+
+const firstSentence= `Ion castiga 5000 lei/luna din salariu, 10000 lei/an bonus si 1500 lei/luna din freelancing.`
+const secondSentence=`Andrei are un salariu de 1000 euro/luna,un bonus de 9000 lei si mai castiga suplimentar aproximativ 500 usd/luna din freelancing.`
+
+const firstSentenceArr= firstSentence.split (``);
+
+console.log(firstSentenceArr)
